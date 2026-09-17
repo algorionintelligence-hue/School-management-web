e8eb0aab7e73c2ce63af9cbfff4e1b0052864fa7
Komal Rai <99144267+KomalRai18@users.noreply.github.com>
2026-09-18 00:58:49 +0500

    fix email config

--- server/src/config/email.config.js
+++ server/src/config/email.config.js
@@ -4,12 +4,15 @@ import nodemailer from "nodemailer";
 const smtpPort = Number(process.env.SMTP_PORT || 587);
 
 export const emailTransporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
+  host: process.env.SMTP_HOST || '127.0.0.1',
   port: smtpPort,
-  secure: false || smtpPort === 465,
-  auth: {
+  secure: process.env.SMTP_SECURE === "true" || smtpPort === 465,
+  auth: (process.env.SMTP_USER && process.env.SMTP_PASS) ? {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
+  } : undefined,
+  tls: {
+    rejectUnauthorized: false, // Accepts self-signed TLS certificates from VPS Postfix/host
   },
 });
 
